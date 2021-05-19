450ad3cad2339dbb3b7c870818624ba9638a5bd8
novokolg <ignateva.olga@gmail.com>
2021-05-20 00:26:25 +0300

    Commit to correct branch homework2

---
+++ online_inference/make_request.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import requests
+import json
+
+if __name__ == "__main__":
+    data = pd.read_csv("data.csv")
+    data['target'] = 0
+    request_features = list(data.columns)
+    print(request_features)
+    for i in range(100):
+        request_data = [
+            x.item() if isinstance(x, np.generic) else x for x in data.iloc[i].tolist()
+        ]
+        print({"data": [request_data], "features": request_features})
+        response = requests.get(
+            "http://127.0.0.1:8000/predict/",
+            #            json={"data": [request_data], "features": request_features},
+            data=json.dumps({"data": [request_data], "features": request_features})
+        )
+        print(str(json.dumps({"data": [request_data], "features": request_features})))
+        print(response.status_code)
+        print(response)
+        print(response.json())
---
+++ online_inference/test_predict.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+from fastapi.testclient import TestClient
+from app import app
+
+os.environ['PATH_TO_MODEL'] = './model.pkl'
+os.environ['PATH_TO_TRANSFORMER'] = './transformer.pkl'
+
+
+def test_read_main():
+    with TestClient(app) as client:
+        response = client.get("/")
+        assert response.status_code == 200
+        assert response.json() == "it is entry point of our predictor"
+
+
+def test_predict():
+    data = pd.read_csv("data.csv")
+    data['target'] = 0
+    with TestClient(app) as client:
+        response = client.get(
+            "/predict",
+            json={"data": data.loc[[0]].values.tolist(), "features": data.columns.tolist()})
+        assert response.status_code == 200
+        assert response.json() == [1]
